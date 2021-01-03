6d5e424b272419c26f2d30ac9bcceff701d21140
Bertone Leonardo <52554768+soubertone@users.noreply.github.com>
2021-01-03 12:36:38 -0300

    funções datas

--- funcoes_datas.php
+++ funcoes_datas.php
@@ -33,6 +33,21 @@
     echo "<br>";
     echo $data_final . " - " . $time_final;
 
+    $diferenca_time = abs($time_final - $time_inicial); //Fazendo a conta para descobrir a diferença. (Sempre timeFinal primeiro)
+
+    echo "<br>";
+    echo $diferenca_time; //Diferença de segundos (Inicial e final)
+
+    $segundos_diarios = 24 * 60 * 60; //Segundos em um dia
+
+    echo "<br>";
+    echo "Existem " . $segundos_diarios . " segundos em um dia.";
+
+    $diferenca_entre_datas = $diferenca_time / $segundos_diarios; //Conta para encontrar quantos entre (TimeFinal e TimeInicial)
+
+    echo "<br>";
+    echo "A diferença é de ". $diferenca_entre_datas . " dias.";
+
 
     ?>
     
