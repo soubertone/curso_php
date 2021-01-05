42555dbe4bcda8de5f2ecb027539439e384ea20b
Bertone Leonardo <52554768+soubertone@users.noreply.github.com>
2021-01-05 11:34:18 -0300

    up for

---
+++ loops_for.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores - Atribuição</title>
+</head>
+<body>
+
+    <?php
+
+         /* while () {
+
+         }       
+
+         do {
+
+         } while ();
+         */
+
+         for($x = 10; $x >= 0; $x--) {
+
+            //break
+            //continue
+
+            echo $x . "<br>";
+         }
+    ?>
+    
+</body>
+</html>
